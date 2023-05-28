55d4d27b3b83d95718f9152e7b89494153227525
Sandeep Kumar <sandeep.lnwebworks@gmail.com>
2023-05-28 22:13:51 +0530

    margin removed from bottom

--- footer.php
+++ footer.php
@@ -1,11 +1,16 @@
-
-<footer class="py-3 my-4 bg-dark">
+<footer class="py-3 bg-dark custom-footer">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="index.php" class="nav-link px-2 text-white">Home</a></li>
-      <li class="nav-item"><a href="aboutme.php" class="nav-link px-2 text-white">About</a></li>
-      <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-white">Contact</a></li>
-      <li class="nav-item"><a href="privacy-policy.php" class="nav-link px-2 text-white">Privacy Policy</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-white">Home</a></li>
+        <li class="nav-item"><a href="aboutme.php" class="nav-link px-2 text-white">About</a></li>
+        <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-white">Contact</a></li>
+        <li class="nav-item"><a href="privacy-policy.php" class="nav-link px-2 text-white">Privacy Policy</a></li>
     </ul>
     <p class="text-center text-white">© <?php echo date('Y'); ?> My Website. All rights reserved. </p>
 </footer>
 
+<style>
+    .custom-footer {
+        margin-top: 2rem; /* Adjust the margin as per your needs */
+        margin-bottom: 0; /* Remove the margin from the bottom */
+    }
+</style>
